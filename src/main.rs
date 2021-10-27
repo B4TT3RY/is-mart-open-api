ad36b9541c225e7cd9865ef5c3143b6573b784a7
B4TT3RY <gb4tt3ry@gmail.com>
2021-10-27 10:59:39 +0900

    Setup base

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    
+use async_std::task;
+use eyre::Result;
+
+fn main() -> Result<()> {
+    task::block_on(start())?;
+    Ok(())
+}
+
+async fn start() -> Result<()> {
+    Ok(())
 }
